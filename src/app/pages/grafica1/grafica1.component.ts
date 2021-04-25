991613c03dca631ad13b2edd3c5169b3feebf940
Juan <gurquiza8@gmail.com>
2021-04-25 14:49:18 -0500

    progress bar y componentes de padres a hijos input y output

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-grafica1',
@@ -6,11 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class Grafica1Component implements OnInit {
 
-  constructor() { }
+export class Grafica1Component {
 
-  ngOnInit(): void {
-  }
+  public labels1: string[] = ['Pan', 'Refresco', 'Tacos'];
+  public data1 = [
+    [10, 15, 40],
+  ];
 
 }
