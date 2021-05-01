28edc9d2283c24f46896e77c6cfc812af400007d
Juan <gurquiza8@gmail.com>
2021-05-01 14:57:10 -0500

    observables y promesas

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -12,8 +12,10 @@ export class SidebarService {
       icono: 'mdi mdi-gauge',
       submenu: [
         { titulo: 'Main', url: '/' },
+        { titulo: 'Gráficas', url: 'grafica1' },
+        { titulo: 'RXJS', url: 'rxjs' },
         { titulo: 'Progressbar', url: 'progress' },
-        { titulo: 'Gráficas', url: 'grafica1' }
+        { titulo: 'Promesas', url: 'promesas' }
       ]
     }
   ];
